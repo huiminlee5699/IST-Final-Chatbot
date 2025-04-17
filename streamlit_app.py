134ba178c95cf525bedd73846b0ac75f9061df79
Hui Min Lee <145514692+huiminlee5699@users.noreply.github.com>
2025-04-17 11:03:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ if prompt := st.chat_input("What would you like to know today?"):
         # If this is an even-numbered assistant message (2nd, 4th, etc.)
         if len(assistant_messages) % 2 == 1:
             full_response += (
-                "\n\n --------------------- \n 💡🧠🤓 <b> Want to learn how I come up with responses? <\b>💡🧠🤓  Visit this link to find out more: "
+                "\n\n --------------------- \n 💡🧠🤓 Want to learn how I come up with responses? 💡🧠🤓  Visit this link to find out more: "
                 "https://ai.meta.com/tools/system-cards/ai-systems-that-generate-text/"
             )
             response_container.markdown(full_response)
