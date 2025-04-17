9c1ea8afbcfc1ae49a8b591ecd16742c043241bf
Hui Min Lee <145514692+huiminlee5699@users.noreply.github.com>
2025-04-17 14:26:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ footer {
     bottom: 0;
     left: 0;
     right: 0;
-    background: #f9f9f9;
+    background: transparent;
     padding: 15px;
     box-shadow: 0 -4px 8px rgba(0, 0, 0, 0.1);
     font-size: 0.9rem;
